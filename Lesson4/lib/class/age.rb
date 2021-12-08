b2b83444d2452028467c497cdf3abdd58ca4fd10
vip <vip1313@gmail.com>
2021-12-09 01:19:41 +0200

    rubocop work

--- Lesson4/lib/class/age.rb
+++ Lesson4/lib/class/age.rb
@@ -1,21 +1,15 @@
 # frozen_string_literal: true
 
-
 class Age
-def self.birthday(date)
-  date_rozd = date.split('.').map(&:to_i)
-  date, month, year = *date_rozd
-  curent_date = Date.today.day
-  curent_month = Date.today.month
-  curent_year = Date.today.year
+  def self.birthday(date)
+    date_rozd = date.split('.').map(&:to_i)
+    date, month, year = *date_rozd
+    curent_date = Date.today.day
+    curent_month = Date.today.month
+    curent_year = Date.today.year
 
-  q = curent_year - year - (curent_month > month || (curent_month == month && curent_date >= date) ? 0 : 1)
+    q = curent_year - year - (curent_month > month || (curent_month == month && curent_date >= date) ? 0 : 1)
 
-  if curent_year >= year
-
-     "Ваш возраст: #{q} "
-  else
-    ' Вы из будущего'
+    curent_year >= year ? "Ваш возраст: #{q}" : 'Вы из будущего'
   end
-end
 end
--- Lesson4/lib/tasks/rbcount.rake
+++ Lesson4/lib/tasks/rbcount.rake
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 desc 'Подсчет кол-ва руби файлов'
 task :rbcount do
-puts Dir.glob('**/*.rb').count
+  puts Dir.glob('**/*.rb').count
 end
